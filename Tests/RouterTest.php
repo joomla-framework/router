c65daac02de0680acf7072e44a06f2c204739b58
Don Gilbert <don@electriceasel.com>
2014-02-09 10:07:46 -0800

    Initial refactor for 2.0

--- Tests/RouterTest.php
+++ Tests/RouterTest.php
@@ -9,11 +9,6 @@ namespace Joomla\Router\Tests;
 use Joomla\Router\Router;
 use Joomla\Test\TestHelper;
 
-require_once __DIR__ . '/Stubs/Bar.php';
-require_once __DIR__ . '/Stubs/Baz.php';
-require_once __DIR__ . '/Stubs/Foo.php';
-require_once __DIR__ . '/Stubs/GooGet.php';
-
 /**
  * Tests for the Joomla\Router\Router class.
  *
@@ -27,76 +22,20 @@ class RouterTest extends \PHPUnit_Framework_TestCase
 	 * @var    Router
 	 * @since  1.0
 	 */
-	private $instance;
+	protected $instance;
 
 	/**
-	 * Provides test data for the testParseRoute method.
-	 *
-	 * @return  array
-	 *
-	 * @since   1.0
-	 */
-	public static function seedTestParseRoute()
-	{
-		// Route, Exception, ControllerName, InputData, MapSet
-		return array(
-			array('', false, 'home', array(), 1),
-			array('articles/4', true, 'home', array(), 1),
-			array('', false, 'index', array(), 2),
-			array('login', false, 'login', array('_rawRoute' => 'login'), 2),
-			array('articles', false, 'articles', array('_rawRoute' => 'articles'), 2),
-			array('articles/4', false, 'article', array('article_id' => 4, '_rawRoute' => 'articles/4'), 2),
-			array('articles/4/crap', true, '', array(), 2),
-			array('test', true, '', array(), 2),
-			array('test/foo', true, '', array(), 2),
-			array('test/foo/path', true, '', array(), 2),
-			array('test/foo/path/bar', false, 'test', array('seg1' => 'foo', 'seg2' => 'bar', '_rawRoute' => 'test/foo/path/bar'), 2),
-			array('content/article-1/*', false, 'content', array('_rawRoute' => 'content/article-1/*'), 2),
-			array('content/cat-1/article-1', false,
-				'article', array('category' => 'cat-1', 'article' => 'article-1', '_rawRoute' => 'content/cat-1/article-1'), 2),
-			array('content/cat-1/cat-2/article-1', false,
-				'article', array('category' => 'cat-1/cat-2', 'article' => 'article-1', '_rawRoute' => 'content/cat-1/cat-2/article-1'), 2),
-			array('content/cat-1/cat-2/cat-3/article-1', false,
-				'article', array('category' => 'cat-1/cat-2/cat-3', 'article' => 'article-1', '_rawRoute' => 'content/cat-1/cat-2/cat-3/article-1'), 2)
-		);
-	}
-
-	/**
-	 * Setup the router maps to option 1.
-	 *
-	 * This has no routes but has a default controller for the home page.
+	 * Prepares the environment before running a test.
 	 *
 	 * @return  void
 	 *
 	 * @since   1.0
 	 */
-	protected function setMaps1()
+	protected function setUp()
 	{
-		$this->instance->addMaps(array());
-		$this->instance->setDefaultController('home');
-	}
+		parent::setUp();
 
-	/**
-	 * Setup the router maps to option 2.
-	 *
-	 * @return  void
-	 *
-	 * @since   1.0
-	 */
-	protected function setMaps2()
-	{
-		$this->instance->addMaps(
-			array(
-				'login' => 'login',
-				'logout' => 'logout',
-				'articles' => 'articles',
-				'articles/:article_id' => 'article',
-				'test/:seg1/path/:seg2' => 'test',
-				'content/:/\*' => 'content',
-				'content/*category/:article' => 'article'
-			)
-		);
-		$this->instance->setDefaultController('index');
+		$this->instance = new Router;
 	}
 
 	/**
@@ -109,7 +48,54 @@ class RouterTest extends \PHPUnit_Framework_TestCase
 	 */
 	public function test__construct()
 	{
-		$this->assertAttributeInstanceOf('Joomla\\Input\\Input', 'input', $this->instance);
+		$this->assertAttributeEmpty('maps', $this->instance);
+	}
+
+	/**
+	 * Tests the Joomla\Router\Router::__construct method.
+	 *
+	 * @return  void
+	 *
+	 * @covers  Joomla\Router\Router::__construct
+	 * @since   1.0
+	 */
+	public function test__constructNotEmpty()
+	{
+		$maps = array(
+			array(
+				'pattern' => 'login',
+				'controller' => 'login'
+			),
+			array(
+				'pattern' => 'requests/:request_id',
+				'controller' => 'request',
+				'rules' => array(
+					'request_id' => '(\d+)'
+				)
+			)
+		);
+
+		$rules = array(
+			array(
+				'regex' => chr(1) . '^login$' . chr(1),
+				'vars' => array(),
+				'controller' => 'login'
+			),
+			array(
+				'regex' => chr(1) . '^requests/(\d+)$' . chr(1),
+				'vars' => array('request_id'),
+				'controller' => 'request'
+			)
+		);
+
+		$router = new Router($maps);
+
+		$this->assertAttributeEquals(
+			 $rules,
+				 'maps',
+				 $router,
+				 'When passing an array of routes when instantiating a Router, the maps property should be set accordingly.'
+		);
 	}
 
 	/**
@@ -125,15 +111,15 @@ class RouterTest extends \PHPUnit_Framework_TestCase
 		$this->assertAttributeEmpty('maps', $this->instance);
 		$this->instance->addMap('foo', 'MyApplicationFoo');
 		$this->assertAttributeEquals(
-			array(
-				array(
-					'regex' => chr(1) . '^foo$' . chr(1),
-					'vars' => array(),
-					'controller' => 'MyApplicationFoo'
-				)
-			),
-			'maps',
-			$this->instance
+			 array(
+				 array(
+					 'regex' => chr(1) . '^foo$' . chr(1),
+					 'vars' => array(),
+					 'controller' => 'MyApplicationFoo'
+				 )
+			 ),
+				 'maps',
+				 $this->instance
 		);
 	}
 
@@ -148,10 +134,24 @@ class RouterTest extends \PHPUnit_Framework_TestCase
 	public function testAddMaps()
 	{
 		$maps = array(
-			'login' => 'login',
-			'logout' => 'logout',
-			'requests' => 'requests',
-			'requests/:request_id' => 'request'
+			array(
+				'pattern' => 'login',
+				'controller' => 'login'
+			),
+			array(
+				'pattern' => 'user/:name/:id',
+				'controller' => 'UserController',
+				'rules' => array(
+					'id' => '(\d+)'
+				)
+			),
+			array(
+				'pattern' => 'requests/:request_id',
+				'controller' => 'request',
+				'rules' => array(
+					'request_id' => '(\d+)'
+				)
+			)
 		);
 
 		$rules = array(
@@ -161,17 +161,15 @@ class RouterTest extends \PHPUnit_Framework_TestCase
 				'controller' => 'login'
 			),
 			array(
-				'regex' => chr(1) . '^logout$' . chr(1),
-				'vars' => array(),
-				'controller' => 'logout'
+				'regex' => chr(1) . '^user/([^/]*)/(\d+)$' . chr(1),
+				'vars' => array(
+					'name',
+					'id'
+				),
+				'controller' => 'UserController'
 			),
 			array(
-				'regex' => chr(1) . '^requests$' . chr(1),
-				'vars' => array(),
-				'controller' => 'requests'
-			),
-			array(
-				'regex' => chr(1) . '^requests/([^/]*)$' . chr(1),
+				'regex' => chr(1) . '^requests/(\d+)$' . chr(1),
 				'vars' => array('request_id'),
 				'controller' => 'request'
 			)
@@ -183,32 +181,11 @@ class RouterTest extends \PHPUnit_Framework_TestCase
 	}
 
 	/**
-	 * Tests the Joomla\Router\Router::getController method.
-	 *
-	 * @return  void
-	 *
-	 * @covers  Joomla\Router\Router::getController
-	 * @since   1.0
-	 */
-	public function testGetController()
-	{
-		$this->instance->setControllerPrefix('\Joomla\Router\Tests\Stubs\\')
-			->addMap('articles/:article_id', 'GooGet');
-
-		$controller = $this->instance->getController('articles/3');
-		$this->assertInstanceOf('\Joomla\Router\Tests\Stubs\GooGet', $controller);
-
-		$input = $controller->getInput();
-		$this->assertEquals('3', $input->get('article_id'));
-	}
-
-	/**
 	 * Tests the Joomla\Router\Router::parseRoute method.
 	 *
 	 * @param   string   $r  The route to parse.
 	 * @param   boolean  $e  True if an exception is expected.
-	 * @param   string   $c  The expected controller name.
-	 * @param   array    $i  The expected input object data.
+	 * @param   array    $i  The expected return data.
 	 * @param   integer  $m  The map set to use for setting up the router.
 	 *
 	 * @return  void
@@ -217,11 +194,10 @@ class RouterTest extends \PHPUnit_Framework_TestCase
 	 * @dataProvider  seedTestParseRoute
 	 * @since         1.0
 	 */
-	public function testParseRoute($r, $e, $c, $i, $m)
+	public function testParseRoute($r, $e, $i, $m)
 	{
 		// Setup the router maps.
-		$mapSetup = 'setMaps' . $m;
-		$this->$mapSetup();
+		$this->{'setMaps' . $m}();
 
 		// If we should expect an exception set that up.
 		if ($e)
@@ -233,117 +209,95 @@ class RouterTest extends \PHPUnit_Framework_TestCase
 		$actual = TestHelper::invoke($this->instance, 'parseRoute', $r);
 
 		// Test the assertions.
-		$this->assertEquals($c, $actual, 'Incorrect controller name found.');
+		$this->assertEquals($i, $actual, 'Incorrect value returned.');
 	}
 
 	/**
-	 * Tests the Joomla\Router\Router::setControllerPrefix method.
+	 * Provides test data for the testParseRoute method.
 	 *
-	 * @return  void
+	 * @return  array
 	 *
-	 * @covers  Joomla\Router\Router::setControllerPrefix
 	 * @since   1.0
 	 */
-	public function testSetControllerPrefix()
+	public static function seedTestParseRoute()
 	{
-		$this->instance->setControllerPrefix('MyApplication');
-		$this->assertAttributeEquals('MyApplication', 'controllerPrefix', $this->instance);
+		// Route Pattern, Throws Exception, Return Data, MapSetup
+		return array(
+			array('', true, array(), 1),
+			array('articles/4', true, array(), 1),
+			array('', true, array(), 2),
+			array('login', false, array('controller' => 'LoginController', 'vars' => array('_rawRoute' => 'login')), 2),
+			array('articles', false, array('controller' => 'ArticlesController', 'vars' => array('_rawRoute' => 'articles')), 2),
+			array('articles/4', false, array('controller' => 'ArticleController', 'vars' => array('article_id' => 4, '_rawRoute' => 'articles/4')), 2),
+			array('articles/4/crap', true, array(), 2),
+			array('test', true, array(), 2),
+			array('test/foo', true, array(), 2),
+			array('test/foo/path', true, array(), 2),
+			array('test/foo/path/bar', false, array('controller' => 'TestController', 'vars' => array('seg1' => 'foo', 'seg2' => 'bar', '_rawRoute' => 'test/foo/path/bar')), 2),
+			array('content/article-1/*', false, array('controller' => 'ContentController', 'vars' => array('_rawRoute' => 'content/article-1/*')), 2),
+			array('content/cat-1/article-1', false,
+				array('controller' => 'ArticleController', 'vars' => array('category' => 'cat-1', 'article' => 'article-1', '_rawRoute' => 'content/cat-1/article-1')), 2),
+			array('content/cat-1/cat-2/article-1', false,
+				array('controller' => 'ArticleController', 'vars' => array('category' => 'cat-1/cat-2', 'article' => 'article-1', '_rawRoute' => 'content/cat-1/cat-2/article-1')), 2),
+			array('content/cat-1/cat-2/cat-3/article-1', false,
+				array('controller' => 'ArticleController', 'vars' => array('category' => 'cat-1/cat-2/cat-3', 'article' => 'article-1', '_rawRoute' => 'content/cat-1/cat-2/cat-3/article-1')), 2)
+		);
 	}
 
 	/**
-	 * Tests the Joomla\Router\Router::setDefaultController method.
+	 * Setup the router maps to option 1.
 	 *
-	 * @return  void
-	 *
-	 * @covers  Joomla\Router\Router::setDefaultController
-	 * @since   1.0
-	 */
-	public function testSetDefaultController()
-	{
-		$this->instance->setDefaultController('foobar');
-		$this->assertAttributeEquals('foobar', 'default', $this->instance);
-	}
-
-	/**
-	 * Tests the Joomla\Router\Router::fetchController method if the controller class is missing.
-	 *
-	 * @return  void
-	 *
-	 * @covers  Joomla\Router\Router::fetchController
-	 * @since   1.0
-	 */
-	public function testFetchControllerWithMissingClass()
-	{
-		$this->setExpectedException('RuntimeException');
-		$controller = TestHelper::invoke($this->instance, 'fetchController', 'goober');
-	}
-
-	/**
-	 * Tests the Joomla\Router\Router::fetchController method if the class not a controller.
-	 *
-	 * @return  void
-	 *
-	 * @covers  Joomla\Router\Router::fetchController
-	 * @since   1.0
-	 */
-	public function testFetchControllerWithNonController()
-	{
-		$this->setExpectedException('RuntimeException');
-		$controller = TestHelper::invoke($this->instance, 'fetchController', 'MyTestControllerBaz');
-	}
-
-	/**
-	 * Tests the Joomla\Router\Router::fetchController method with a prefix set.
-	 *
-	 * @return  void
-	 *
-	 * @covers  Joomla\Router\Router::fetchController
-	 * @since   1.0
-	 */
-	public function testFetchControllerWithPrefixSet()
-	{
-		$this->instance->setControllerPrefix('MyTestController');
-		$controller = TestHelper::invoke($this->instance, 'fetchController', 'Foo');
-	}
-
-	/**
-	 * Tests the Joomla\Router\Router::fetchController method without a prefix set even though it is necessary.
-	 *
-	 * @return  void
-	 *
-	 * @covers  Joomla\Router\Router::fetchController
-	 * @since   1.0
-	 */
-	public function testFetchControllerWithoutPrefixSetThoughNecessary()
-	{
-		$this->setExpectedException('RuntimeException');
-		$controller = TestHelper::invoke($this->instance, 'fetchController', 'foo');
-	}
-
-	/**
-	 * Tests the Joomla\Router\Router::fetchController method without a prefix set.
-	 *
-	 * @return  void
-	 *
-	 * @covers  Joomla\Router\Router::fetchController
-	 * @since   1.0
-	 */
-	public function testFetchControllerWithoutPrefixSet()
-	{
-		$controller = TestHelper::invoke($this->instance, 'fetchController', 'TControllerBar');
-	}
-
-	/**
-	 * Prepares the environment before running a test.
+	 * This has no routes but has a default controller for the home page.
 	 *
 	 * @return  void
 	 *
 	 * @since   1.0
 	 */
-	protected function setUp()
+	protected function setMaps1()
 	{
-		parent::setUp();
+		$this->instance->addMaps(array());
+	}
 
-		$this->instance = new Router;
+	/**
+	 * Setup the router maps to option 2.
+	 *
+	 * @return  void
+	 *
+	 * @since   1.0
+	 */
+	protected function setMaps2()
+	{
+		$this->instance->addMaps(
+			array(
+				array(
+				   'pattern' => 'login',
+				   'controller' => 'LoginController'
+				),
+				array(
+				   'pattern' => 'logout',
+				   'controller' => 'LogoutController'
+				),
+				array(
+				   'pattern' => 'articles',
+				   'controller' => 'ArticlesController'
+				),
+				array(
+				   'pattern' => 'articles/:article_id',
+				   'controller' => 'ArticleController'
+				),
+				array(
+				   'pattern' => 'test/:seg1/path/:seg2',
+				   'controller' => 'TestController'
+				),
+				array(
+				   'pattern' => 'content/:/\*',
+				   'controller' => 'ContentController'
+				),
+				array(
+				   'pattern' => 'content/*category/:article',
+				   'controller' => 'ArticleController'
+				)
+			)
+		);
 	}
 }
--- src/Router.php
+++ src/Router.php
@@ -8,9 +8,6 @@
 
 namespace Joomla\Router;
 
-use Joomla\Controller\ControllerInterface;
-use Joomla\Input\Input;
-
 /**
  * A path router.
  *
@@ -18,30 +15,6 @@ use Joomla\Input\Input;
  */
 class Router
 {
-	/**
-	 * Controller class name prefix for creating controller objects by name.
-	 *
-	 * @var    string
-	 * @since  1.0
-	 */
-	protected $controllerPrefix;
-
-	/**
-	 * The default page controller name for an empty route.
-	 *
-	 * @var    string
-	 * @since  1.0
-	 */
-	protected $default;
-
-	/**
-	 * An input object from which to derive the route.
-	 *
-	 * @var    Input
-	 * @since  1.0
-	 */
-	protected $input;
-
 	/**
 	 * An array of rules, each rule being an associative array('regex'=> $regex, 'vars' => $vars, 'controller' => $controller)
 	 * for routing the request.
@@ -54,27 +27,30 @@ class Router
 	/**
 	 * Constructor.
 	 *
-	 * @param   Input  $input  An optional input object from which to derive the route.  If none
-	 *                         is given than the input from the application object will be used.
+	 * @param   array  $maps  An optional array of route maps
 	 *
 	 * @since   1.0
 	 */
-	public function __construct(Input $input = null)
+	public function __construct(array $maps = array())
 	{
-		$this->input = ($input === null) ? new Input : $input;
+		if (! empty($maps))
+		{
+			$this->addMaps($maps);
+		}
 	}
 
 	/**
 	 * Add a route map to the router. If the pattern already exists it will be overwritten.
 	 *
 	 * @param   string  $pattern     The route pattern to use for matching.
-	 * @param   string  $controller  The controller name to map to the given pattern.
+	 * @param   mixed   $controller  The controller to map to the given pattern.
+	 * @parem   array   $rules       An array of regex rules keyed using the route variables.
 	 *
 	 * @return  Router  Returns itself to support chaining.
 	 *
 	 * @since   1.0
 	 */
-	public function addMap($pattern, $controller)
+	public function addMap($pattern, $controller, array $rules = array())
 	{
 		// Sanitize and explode the pattern.
 		$pattern = explode('/', trim(parse_url((string) $pattern, PHP_URL_PATH), ' /'));
@@ -112,8 +88,10 @@ class Router
 			elseif ($segment[0] == ':')
 			{
 				// Match a named variable and capture the data.
-				$vars[] = substr($segment, 1);
-				$regex[] = '([^/]*)';
+				$varName = substr($segment, 1);
+				$vars[] = $varName;
+				// Use the regex in the rules array if it has been defined.
+				$regex[] = array_key_exists($varName, $rules) ? $rules[$varName] : '([^/]*)';
 			}
 			elseif ($segment[0] == '\\' && $segment[1] == ':')
 			{
@@ -130,7 +108,7 @@ class Router
 		$this->maps[] = array(
 			'regex' => chr(1) . '^' . implode('/', $regex) . '$' . chr(1),
 			'vars' => $vars,
-			'controller' => (string) $controller
+			'controller' => $controller
 		);
 
 		return $this;
@@ -143,95 +121,33 @@ class Router
 	 *
 	 * @return  Router  Returns itself to support chaining.
 	 *
+	 * @throws  \UnexpectedValueException  If missing the `pattern` or `controller` keys from the map.
+	 *
 	 * @since   1.0
 	 */
 	public function addMaps($maps)
 	{
-		foreach ($maps as $pattern => $controller)
+		foreach ($maps as $map)
 		{
-			$this->addMap($pattern, $controller);
+			// Ensure a `pattern` key exists
+			if (! array_key_exists('pattern', $map))
+			{
+				throw new \UnexpectedValueException('Route map must contain a pattern variable.');
+			}
+
+			// Ensure a `controller` key exists
+			if (! array_key_exists('controller', $map))
+			{
+				throw new \UnexpectedValueException('Route map must contain a controller variable.');
+			}
+
+			// If rules have been specified, add them as well.
+			$rules = array_key_exists('rules', $map) ? $map['rules'] : array();
+
+			$this->addMap($map['pattern'], $map['controller'], $rules);
 		}
 
 		return $this;
-	}
-
-	/**
-	 * Find and execute the appropriate controller based on a given route.
-	 *
-	 * @param   string  $route  The route string for which to find and execute a controller.
-	 *
-	 * @return  ControllerInterface
-	 *
-	 * @since   1.0
-	 * @throws  \InvalidArgumentException
-	 * @throws  \RuntimeException
-	 */
-	public function getController($route)
-	{
-		// Get the controller name based on the route patterns and requested route.
-		$name = $this->parseRoute($route);
-
-		// Get the controller object by name.
-		return $this->fetchController($name);
-	}
-
-	/**
-	 * Set the controller name prefix.
-	 *
-	 * @param   string  $prefix  Controller class name prefix for creating controller objects by name.
-	 *
-	 * @return  Router  Returns itself to support chaining.
-	 *
-	 * @since   1.0
-	 */
-	public function setControllerPrefix($prefix)
-	{
-		$this->controllerPrefix	= (string) $prefix;
-
-		return $this;
-	}
-
-	/**
-	 * Set the default controller name.
-	 *
-	 * @param   string  $name  The default page controller name for an empty route.
-	 *
-	 * @return  Router  Returns itself to support chaining.
-	 *
-	 * @since   1.0
-	 */
-	public function setDefaultController($name)
-	{
-		$this->default = (string) $name;
-
-		return $this;
-	}
-
-	/**
-	 * Get a JController object for a given name.
-	 *
-	 * @param   string  $name  The controller name (excluding prefix) for which to fetch and instance.
-	 *
-	 * @return  ControllerInterface
-	 *
-	 * @since   1.0
-	 * @throws  \RuntimeException
-	 */
-	protected function fetchController($name)
-	{
-		// Derive the controller class name.
-		$class = $this->controllerPrefix . ucfirst($name);
-
-		// If the controller class does not exist panic.
-		if (!class_exists($class) || !is_subclass_of($class, 'Joomla\\Controller\\ControllerInterface'))
-		{
-			throw new \RuntimeException(sprintf('Unable to locate controller `%s`.', $class), 404);
-		}
-
-		// Instantiate the controller.
-		$controller = new $class($this->input);
-
-		return $controller;
 	}
 
 	/**
@@ -239,26 +155,18 @@ class Router
 	 *
 	 * @param   string  $route  The route string for which to find and execute a controller.
 	 *
-	 * @return  string  The controller name for the given route excluding prefix.
+	 * @return  array   An array containing the controller and the matched variables.
 	 *
 	 * @since   1.0
 	 * @throws  \InvalidArgumentException
 	 */
-	protected function parseRoute($route)
+	public function parseRoute($route)
 	{
-		$controller = false;
-
 		// Trim the query string off.
 		$route = preg_replace('/([^?]*).*/u', '\1', $route);
 
 		// Sanitize and explode the route.
 		$route = trim(parse_url($route, PHP_URL_PATH), ' /');
-
-		// If the route is empty then simply return the default route.  No parsing necessary.
-		if ($route == '')
-		{
-			return $this->default;
-		}
 
 		// Iterate through all of the known route maps looking for a match.
 		foreach ($this->maps as $rule)
@@ -266,30 +174,22 @@ class Router
 			if (preg_match($rule['regex'], $route, $matches))
 			{
 				// If we have gotten this far then we have a positive match.
-				$controller = $rule['controller'];
+				$vars = array();
 
-				// Time to set the input variables.
-				// We are only going to set them if they don't already exist to avoid overwriting things.
 				foreach ($rule['vars'] as $i => $var)
 				{
-					$this->input->def($var, $matches[$i + 1]);
-
-					// Don't forget to do an explicit set on the GET superglobal.
-					$this->input->get->def($var, $matches[$i + 1]);
+					$vars[$var] = $matches[$i + 1];
 				}
 
-				$this->input->def('_rawRoute', $route);
+				$vars['_rawRoute'] = $route;
 
-				break;
+				return array(
+					'controller' => $rule['controller'],
+					'vars' => $vars
+				);
 			}
 		}
 
-		// We were unable to find a route match for the request.  Panic.
-		if (!$controller)
-		{
-			throw new \InvalidArgumentException(sprintf('Unable to handle request for route `%s`.', $route), 404);
-		}
-
-		return $controller;
+		throw new \InvalidArgumentException(sprintf('Unable to handle request for route `%s`.', $route), 404);
 	}
 }
